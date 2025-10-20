934ec0a8be7d088430dc92f7d3fb45e35e35a73b
V-1-P-3-R <jariasr1@unicartagena.edu.co>
2025-10-20 18:17:23 -0500

    Update experienceRoutes.js

--- routers/experienceRoutes.js
+++ routers/experienceRoutes.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const ctrl = require('../controllers/experienceCtrl');
+
+router.get('/', ctrl.getExperiences);
+router.post('/', ctrl.createExperience);
+router.get('/:id', ctrl.getExperienceById);
+router.put('/:id', ctrl.updateExperience);
+router.delete('/:id', ctrl.deleteExperience);
+
+module.exports = router;
 
