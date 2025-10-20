4f126960e3efce4bcfc1b315d6607a18596abb73
V-1-P-3-R <jariasr1@unicartagena.edu.co>
2025-10-20 18:17:45 -0500

    Initialize Swagger with API documentation
    Set up Swagger documentation for CV Experiences API.

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -1,1 +1,24 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'CV Experiences API',
+      version: '1.0.0',
+      description: 'CRUD de experiencias profesionales para hoja de vida.'
+    },
+    servers: [{ url: 'http://localhost:4000' }]
+  },
+  apis: ['./routes/*.js']
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+function setupSwagger(app) {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+}
+
+module.exports = setupSwagger;
 
