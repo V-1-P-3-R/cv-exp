8dde9611fe0b4c2465df36affd9df3817cdbac4b
V-1-P-3-R <jariasr1@unicartagena.edu.co>
2025-10-20 18:16:25 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,1 +1,14 @@
+const mongoose = require('mongoose');
+
+async function connectDB(uri) {
+  try {
+    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log('✅ MongoDB conectado');
+  } catch (err) {
+    console.error('❌ Error conectando a MongoDB', err);
+    process.exit(1);
+  }
+}
+
+module.exports = connectDB;
 
